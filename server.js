6b40d961ed78766e5f09369ad31caac9a18706b6
Ayyappa818 <ayyappa666swamy@gmail.com>
2024-12-13 11:04:07 +0530

    changes

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ app.use(bodyParser.json())
 
 var model = require('./models/EventCollection.model')
 var Movies = require('./models/MovieCollection.model')
-var mongoose = require('mongoose')
+var mongoose = require('mongoose');
+const { title } = require("process");
 mongoose.connect("mongodb+srv://Ayyappa:ayyappa666@cluster0.m2y7b.mongodb.net/EventCollection?retryWrites=true&w=majority&appName=Cluster0")
 .then((data)=>{console.log("Connected")})
 .catch((err)=>{console.log("Not Connected")})
@@ -36,6 +37,21 @@ app.post('/addevents',(req,res)=>{
     .catch((err)=>{console.log(err)})
 })
 
+app.post('/efilter',async(req,res)=>{
+    console.log(req.body)
+    var data = await model.find({$or:req.body})
+        res.json(data)
+})
+
+app.get('/efilter/:id',(req,res)=>{
+    console.log(req.params.title)
+    model.find({title:req.params.title})
+    .then((data)=>{
+        console.log(data)
+        res.json(data)
+    })
+})
+
 app.get('/movies',(req,res)=>{
     Movies.find()
     .then((data)=>{
@@ -59,6 +75,24 @@ app.post('/addmovies',(req,res)=>{
     var newMovies = new Movies(req.body);
     newMovies.save().then((data)=>{console.log(data)})
     .catch((err)=>{console.log(err)})
+})
+
+app.post('/mfilter',async(req,res)=>{
+    console.log(req.body)
+    var data = await Movies.find({$or:req.body})
+    res.json(data)
+})
+
+app.get('/mfilter/:id',async(req,res)=>{
+    console.log(req.params.title)
+    try{
+        const data = await Movies.find({movieTitle:req.params.title})
+        console.log("responsedata::"+data)
+        res.json(data)
+    }
+    catch(err){
+        console.log("data not found")
+    }
 })
 
 app.listen(6400,()=>{
